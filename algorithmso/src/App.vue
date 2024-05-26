de73fe98b7daee2d71467756a420ce2d35d24d6c
Cindy Marcela Jiménez Saldarriaga <cindy.jimenez@utp.edu.co>
2024-05-25 23:00:27 -0500

    "Base de ALgoritmo estable"

---
+++ algorithmso/src/App.vue
@@ -0,0 +1,36 @@
+<template>
+  <img alt="Vue logo" src="./assets/logo.png">
+  <div id="app">
+    <ScanChart :requests="requests" :start="start" :direction="direction" />
+  </div>
+</template>
+
+<script>
+//import HelloWorld from './components/HelloWorld.vue'
+import ScanChart from './components/ScanChart.vue';
+
+export default {
+  name: 'App',
+  components: { 
+    ScanChart
+  },
+  data() {
+    return {
+      requests: [98, 183, 37, 122, 14, 124, 65, 67],
+      start: 53,
+      direction: 'right'
+    };
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
